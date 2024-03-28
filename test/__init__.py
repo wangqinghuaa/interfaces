8f378fbc57ebfb359dbbf26cba0616867dd39b8f
wangqinghua <867075698@qq.com>
2024-03-28 09:25:50 +0800

    first

---
+++ test/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# @Time     : 2024-03-28 9:23
+# @Author   : 华
+# @Email    : 867075698@qq.com
+# @File     : __init__.py.py
+# @Software : PyCharm
---
+++ test/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# @Time     : 2024-03-28 9:23
+# @Author   : 华
+# @Email    : 867075698@qq.com
+# @File     : test.py
+# @Software : PyCharm
